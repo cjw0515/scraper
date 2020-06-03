9dee6610d3a507422dadf995bec9c286d9cbea33
jongwon <cjw0515@naver.com>
2020-06-03 17:59:07 +0900

    세팅

--- naver_scrap/naver_scrap/settings.py
+++ naver_scrap/naver_scrap/settings.py
@@ -8,18 +8,27 @@
 #     https://docs.scrapy.org/en/latest/topics/settings.html
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+from dotenv import load_dotenv, find_dotenv
+import os
+load_dotenv(find_dotenv())
 
 BOT_NAME = 'naver_scrap'
 
 SPIDER_MODULES = ['naver_scrap.spiders']
 NEWSPIDER_MODULE = 'naver_scrap.spiders'
 
-ITEM_PIPELINES = {
-   'naver_scrap.pipelines.NaverScrapPipeline': 300,
-}
+# ITEM_PIPELINES = {
+#    'naver_scrap.pipelines.NaverScrapPipeline': 300,
+# }
 
-AWS_ACCESS_KEY_ID = 'AKIAIZJ6FGUDMED7YKJA'
-AWS_SECRET_ACCESS_KEY= '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'
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+BUCKET_NAME = 'bi-temp'
+BUCKET_PREFIX = 'datalake/crawling/'
+
+LOG_LEVEL = 'ERROR'
+# LOG_LEVEL = 'INFO'
 
 # FEEDS = {
 #     's3://bi-temp/datalake/crawling/test.json': {
@@ -52,7 +61,7 @@ AWS_SECRET_ACCESS_KEY= 'zFWmGLz38+tIQF/Cu7aBj9XfZWuqDPMNllhJWJ5Y'
 #USER_AGENT = 'naver_scrap (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -60,13 +69,13 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -79,15 +88,16 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'naver_scrap.middlewares.NaverScrapSpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+   'naver_scrap.middlewares.NaverScrapSpiderMiddleware': 543,
+}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'naver_scrap.middlewares.NaverScrapDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   # 'naver_scrap.middlewares.NaverScrapDownloaderProxyMiddleware': 543,
+   'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -100,14 +110,14 @@ ROBOTSTXT_OBEY = True
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
 #AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
