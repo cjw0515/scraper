de38e26a34f8c0459d22285d81765ac96d008968
jongwon <cjw0515@naver.com>
2020-06-16 13:09:31 +0900

    세팅값 추가

--- naver_scrap/naver_scrap/settings.py
+++ naver_scrap/naver_scrap/settings.py
@@ -9,10 +9,21 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 import sys, os
+
 sys.path.append("C:/anaconda3/envs/scraper-10x10/Lib/site-packages")
+# 배포용
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
+
+from ec2_metadata import ec2_metadata
+
+try:
+    INSTANCE_ID = ec2_metadata.instance_id
+except Exception as e:
+    INSTANCE_ID = 'local-pc'
+
 BOT_NAME = 'naver_scrap'
+BOT_ID = ''
 
 SPIDER_MODULES = ['naver_scrap.spiders']
 NEWSPIDER_MODULE = 'naver_scrap.spiders'
@@ -20,12 +31,19 @@ NEWSPIDER_MODULE = 'naver_scrap.spiders'
 # ITEM_PIPELINES = {
 #    'naver_scrap.pipelines.NaverScrapPipeline': 300,
 # }
-
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+# AWS_ACCESS_KEY_ID = 'AKIAIZJ6FGUDMED7YKJA'
+# AWS_SECRET_ACCESS_KEY = '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'
 
 BUCKET_NAME = 'bi-temp'
 BUCKET_PREFIX = 'datalake/crawling/'
+
+if INSTANCE_ID == 'local-pc':
+    FILE_REMOVE = False
+else:
+    FILE_REMOVE = True
+
 
 # LOG_LEVEL = 'ERROR'
 LOG_LEVEL = 'INFO'
@@ -58,51 +76,56 @@ LOG_LEVEL = 'INFO'
 # }
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'naver_scrap (+http://www.yourdomain.com)'
+# USER_AGENT = 'naver_scrap (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
 COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
+# retry
+RETRY_ENABLED = True
+RETRY_TIMES = 5
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 SPIDER_MIDDLEWARES = {
-   'naver_scrap.middlewares.NaverScrapSpiderMiddleware': 543,
+    'naver_scrap.middlewares.NaverScrapSpiderMiddleware': 543,
 }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-   # 'naver_scrap.middlewares.NaverScrapDownloaderProxyMiddleware': 543,
-   'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110
+    'naver_scrap.middlewares.NaverScrapDownloaderMiddleware': 543,
+    'naver_scrap.middlewares.SleepRetryMiddleware': 100,
+    # 'naver_scrap.middlewares.NaverScrapDownloaderProxyMiddleware': 543,
+    # 'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110
 }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
@@ -119,12 +142,12 @@ DOWNLOADER_MIDDLEWARES = {
 # AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
