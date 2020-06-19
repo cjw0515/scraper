86c14ce996147d99b142cf676e74069e516ecb44
jongwon <cjw0515@naver.com>
2020-06-19 16:22:17 +0900

    테스트

---
+++ naver_scrap/naver_scrap/test.py
@@ -0,0 +1,12 @@
+import pythena
+import boto3
+
+athena_client = pythena.Athena(database='tenbyten', region='ap-northeast-2', session=boto3.session.Session())
+sql = 'select * from nvshop_best_brand where year = 2020 and month = 6 and day = 18 limit 10;'
+df = athena_client.execute(sql)
+# li = list(df)
+print(df)
+print(type(df[0].loc[0]['keyword']))
+li = df[0]['keyword'].tolist()
+print(type(list))
+print(li)
