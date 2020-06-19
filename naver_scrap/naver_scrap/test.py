6c69354026480cca30b74b8dcee3f17f3841365f
jongwon <cjw0515@naver.com>
2020-06-19 16:30:12 +0900

    태테스트

--- naver_scrap/naver_scrap/test.py
+++ naver_scrap/naver_scrap/test.py
@@ -1,7 +1,11 @@
 import pythena
 import boto3
 
-athena_client = pythena.Athena(database='tenbyten', region='ap-northeast-2', session=boto3.session.Session())
+athena_client = pythena.Athena(database='tenbyten'
+                               , region='ap-northeast-2'
+                               , session=boto3.session.Session(aws_access_key_id="AKIAIZJ6FGUDMED7YKJA"
+                                                               , aws_secret_access_key="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")
+                               )
 sql = 'select * from nvshop_best_brand where year = 2020 and month = 6 and day = 18 limit 10;'
 df = athena_client.execute(sql)
 # li = list(df)
