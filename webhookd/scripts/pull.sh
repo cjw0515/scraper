be312abd747242136155c5274b64e9426c257f43
jongwon <cjw0515@naver.com>
2020-07-10 10:13:31 +0900

    쉘 스크립트 수정

--- webhookd/scripts/pull.sh
+++ webhookd/scripts/pull.sh
@@ -4,6 +4,6 @@ cd ~/scraper/
 git pull origin master
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate scraper-10x10
-scrapyd-deploy naver_scrap
-scrapyd-deploy lightweight_scrap
+scrapyd-deploy naver_scrap -p naver_scrap
+scrapyd-deploy lightweight_scrap -p lightweight_scrap
 
