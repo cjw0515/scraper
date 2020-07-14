2a2c0122116a7822f8ec9adf05d955b9fb712ba8
jongwon <cjw0515@naver.com>
2020-07-14 17:00:14 +0900

    쉘 스크립트 수정

--- webhookd/scripts/restart.sh
+++ webhookd/scripts/restart.sh
@@ -4,6 +4,6 @@ cd ~/scraper/
 killall scrapyd
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate scraper-10x10
+nohup scrapyd &
 scrapyd-deploy naver_scrap -p naver_scrap
 scrapyd-deploy lightweight_scrap -p lightweight_scrap
-nohup scrapyd &
